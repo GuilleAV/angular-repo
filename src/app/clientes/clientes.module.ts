9bea62c50da3ea01585e73d51755556738d46cb2
gvallejos <guillermo.andres.vallejos@hotmail.com>
2019-10-03 22:01:14 -0300

    Armado de estructura

---
+++ src/app/clientes/clientes.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {FormsModule} from '@angular/forms';
+
+import {ClientesService} from './clientes.service';
+import { AltaClienteComponent } from './alta-cliente/alta-cliente.component';
+import { ListadoClientesComponent } from './listado-clientes/listado-clientes.component';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
+  declarations: [AltaClienteComponent, ListadoClientesComponent],
+  providers: [
+    ClientesService
+  ],
+  exports:[AltaClienteComponent,
+  ListadoClientesComponent]
+})
+export class ClientesModule { }
